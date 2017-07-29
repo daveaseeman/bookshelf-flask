2c76692953ab8e14830176081597834b1ef30449
Dave Seeman <daveseeman@icloud.com>
2017-07-29 12:42:24 -0500

    switched to heroku

--- config.py
+++ config.py
@@ -1,21 +1,12 @@
 import os
 
-# The secret key is used by Flask to encrypt session cookies.
-SECRET_KEY = 'secret'
+SECRET_KEY = os.environ['SECRET']
 
-# There are three different ways to store the data in the application.
-# You can choose 'datastore', 'cloudsql', or 'mongodb'. Be sure to
-# configure the respective settings for the one you choose below.
-# You do not have to configure the other data backends. If unsure, choose
-# 'datastore' as it does not require any additional configuration.
-DATA_BACKEND = 'mongodb'
+DATA_BACKEND = os.environ['BACKEND']
 
-# Google Cloud Project ID. This can be found on the 'Overview' page at
-# https://console.developers.google.com
-PROJECT_ID = os.environ['PROJECT_ID']
+# Google Cloud Project ID.
+# PROJECT_ID = os.environ['PROJECT_ID']
 
 # Mongo configuration
-# If using mongolab, the connection URI is available from the mongolab control
-# panel. If self-hosting on compute engine, replace the values below.
 MONGO_DBNAME = os.environ['MONGO_DBNAME']
 MONGO_URI = os.environ['MONGO_URI']
